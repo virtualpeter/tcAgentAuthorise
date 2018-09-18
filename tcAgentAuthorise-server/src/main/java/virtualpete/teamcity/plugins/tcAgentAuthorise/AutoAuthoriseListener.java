8edf80f617238fbf6b7dc12ac079dcee31632466
Peter Viertel <peter@viertel.org>
2018-09-18 11:24:52 +1000

    sort out semantics - choose better parameter keys

--- tcAgentAuthorise-server/src/main/java/virtualpete/teamcity/plugins/tcAgentAuthorise/AutoAuthoriseListener.java
+++ tcAgentAuthorise-server/src/main/java/virtualpete/teamcity/plugins/tcAgentAuthorise/AutoAuthoriseListener.java
@@ -10,7 +10,39 @@ import org.jetbrains.annotations.NotNull;
 
 /*
  * you might want some agents to be automatically authorised and this plugin will do that
+ * 
+ * the agent needs to be configured with a property to be acted upon.
+ * 
+ * This early version doesnt check who it is so it breaks the security model of teamcity - later version will
+ * have an authorisation token mechanism or something you can at least keep secret but for now if you install
+ * this code you'd be wanting to make sure its in appropriately secured networks. ie, not on the internet.
+ * 
+ * If parameter present it will autoauthorise into default pool
+ * If parameter value is 'static' then it will not automatically deauthorise - so you can have a long running agent that
+ * auto authorises and can come and go without destroying history - good for laptops or systems on a schedule.
+ * 
+ * eg:
+ * 
+ * agent_auto_authorise=true
+ * 
+ * or
+ * 
+ * agent_auto_authorise=static
+ * 
+ * 
+ * Just deauthorise:
+ * 
+ * If parameter present then an agent can be deauthorised automnatically without allowing it to be auto-authorised - this 
+ * case can be good if you still want to authorise agents yourself but tire of having to tidy up old ones.
+ * 
+ * eg:
+ * 
+ * agent_auto_undiscover=true
+ * 
+ * and no - i have not yet figured out how to make it join a specific pool - this is possible, just not easy to figure out 
+ * from the doco.
  */
+
 
 public class AutoAuthoriseListener extends BuildServerAdapter {
 
@@ -26,25 +58,35 @@ public class AutoAuthoriseListener extends BuildServerAdapter {
 
   @Override
   public void agentRegistered(@NotNull SBuildAgent sBuildAgent, long l) {
-    Map<String,String> parameters = sBuildAgent.getAvailableParameters();
-    Loggers.SERVER.info("AutoAuthoriseListener has noticed an Agent Registration");
-    if (parameters.containsKey("auto_authorise_pool")) {
+    Loggers.AGENT.debug("AutoAuthoriseListener has noticed an Agent registration:" + sBuildAgent.getHostName());
+
+    Map<String, String> parameters = sBuildAgent.getAvailableParameters();
+
+    boolean unReady = (sBuildAgent.isOutdated() || sBuildAgent.isUpgrading());
+    boolean autoAuth = ( parameters.get("agent_auto_authorise").equals("static") || parameters.get("agent_auto_authorise").equals("true") );
+    boolean notAuthorised = !sBuildAgent.isAuthorized();
+
+    if (!unReady && notAuthorised && autoAuth) {
       sBuildAgent.setAuthorized(true, null, "autoAuthorise agent registered");
-      Loggers.SERVER.info("AutoAuthoriseListener has set to authorised: " + sBuildAgent.getHostName());
+      Loggers.AGENT.info("AutoAuthoriseListener has set to authorised: " + sBuildAgent.getHostName());
     }
   }
 
   @Override
   public void agentUnregistered(@NotNull SBuildAgent sBuildAgent) {
+    
+    Loggers.AGENT.debug("AutoAuthoriseListener has noticed an Agent deregistration:" + sBuildAgent.getHostName());
     Map<String,String> parameters = sBuildAgent.getAvailableParameters();
-    Loggers.SERVER.info("AutoAuthoriseListener has noticed an Agent Deregistration");
-    if (parameters.containsKey("auto_unDiscover")) {
-      sBuildAgent.setAuthorized(false, null, "autoAuthorise agent auto forgotten");
-      Loggers.SERVER.info("AutoAuthoriseListener has auto undiscovered: " + sBuildAgent.getHostName());
-    }
-    if (parameters.containsKey("auto_deAuthorise") && parameters.get("auto_deAuthorise").equals("true")) {
-      sBuildAgent.setAuthorized(false, null, "autoAuthorise agent has unregistered so will be deAuthorised");
-      Loggers.SERVER.info("AutoAuthoriseListener has auto unregistered: " + sBuildAgent.getHostName());
+
+    if(sBuildAgent.isAuthorized()) {
+      if (parameters.get("agent_auto_undiscover").equals("true")) {
+        sBuildAgent.setAuthorized(false, null, "autoAuthorise agent auto forgotten");
+        Loggers.AGENT.info("AutoAuthoriseListener has auto undiscovered: " + sBuildAgent.getHostName());
+      }
+      if (parameters.containsKey("agent_auto_authorise") && !parameters.get("agent_auto_authorise").equals("static")) {
+        sBuildAgent.setAuthorized(false, null, "autoAuthorise agent has unregistered and will be deAuthorised");
+        Loggers.AGENT.info("AutoAuthoriseListener has auto unregistered: " + sBuildAgent.getHostName());
+      }
     }
   }
 }
